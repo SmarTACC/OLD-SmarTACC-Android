c76f7b248741616e31e01134b849c44479794aa3
pablokvitca <pablokvitca@gmail.com>
2015-12-13 18:53:46 -0300

    Idem
    Idem

--- app/src/main/java/com/ort/smartacc/Util.java
+++ app/src/main/java/com/ort/smartacc/Util.java
@@ -12,7 +12,8 @@ public class Util {
     /**
      * Dirección básica del servidor.
      */
-    static String SERVER_URL = "http://www.ort.edu.ar:8018/";
+    //TODO: cambiar esto por la url de ORT
+    static String SERVER_URL = "http://santiaranguri.com/";
     /**
      * Función que verifica el poder conectarse a internet
      * @param ctx El contexto usado para conseguir el servicio para checkear el estado de la red.
